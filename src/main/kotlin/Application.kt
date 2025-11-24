4684d18be506ac64d06d29e8a784ce3e96e8e0a2
Valerio <m.valerio9109@gmail.com>
2025-11-24 16:35:42 +0100

    change service port

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -6,6 +6,10 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.contentnegotiation.*
 
+
+//https://raspi.tail0458e4.ts.net
+
+
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }

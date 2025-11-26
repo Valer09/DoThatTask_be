eb55ae51d2e1a9389efa92532121c9d7f06420f6
Valerio <m.valerio9109@gmail.com>
2025-11-26 19:10:32 +0100

    cleaning

--- src/main/kotlin/Model/Task.kt
+++ src/main/kotlin/Model/Task.kt
@@ -1,28 +1,6 @@
 package homeaq.dothattask.Model
 
 import kotlinx.serialization.Serializable
-
-enum class TaskStatus(val code: Int) {
-    TODO(1),
-    ACTIVE(2),
-    COMPLETED(3);
-
-    companion object {
-        fun fromCode(code: Int) = entries.first { it.code == code }
-    }
-}
-
-enum class TaskCategory(val code: Int)
-{
-    Social(1),
-    Career(2),
-    Health(3);
-
-    companion object {
-        fun fromCode(code: Int) = entries.first { it.code == code }
-    }
-}
-
 @Serializable
 data class Task
     (
@@ -45,3 +23,24 @@ data class TaskUpdate
     val category: TaskCategory,
     val status: TaskStatus
 )
+
+enum class TaskStatus(val code: Int) {
+    TODO(1),
+    ACTIVE(2),
+    COMPLETED(3);
+
+    companion object {
+        fun fromCode(code: Int) = entries.first { it.code == code }
+    }
+}
+
+enum class TaskCategory(val code: Int)
+{
+    Social(1),
+    Career(2),
+    Health(3);
+
+    companion object {
+        fun fromCode(code: Int) = entries.first { it.code == code }
+    }
+}
